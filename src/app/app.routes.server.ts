7a008a2ca6f7f7e6d8ac472218a399858c8ece2c
= <moabdelaziem@gmail.com>
2026-02-20 02:43:22 +0200

    updateddd

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '/api/*', // أي API route يخليها server
     renderMode: RenderMode.Server
+  },
+   {
+    path: '**', // أي حاجة تانية تروح لل Angular app
+    renderMode: RenderMode.Client
   }
 ];
